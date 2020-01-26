372d81491420e50eb317dfbcf340bd5779f721a9
Darko Nedelkovski <darko11nedelkovski@gmail.com>
2020-01-26 16:49:32 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,51 @@
 $(document).ready(function () {
 
+  $('.navbar-toggler').click(function () {
+
+    $('.navbar-toggler').toggleClass('change')
+
+  })
+
+  $(window).scroll(function(){
+
+    let position = $(this).scrollTop();
+    
+    if(position>= 718){
+      $('.nvabar').addClass('navbar-background');
+      $('.nvabar').addClass('fixed-top');
+    }
+    else{
+      $('.nvabar').removeClass('navbar-background');
+      $('.nvabar').removeClass('fixed-top');
+    }
+  })
+
+  $('.nava-item a .header-link, #back-to-top').click(function(link){ 
+    link.preventDefault();
+
+    let target = $(this).attr('href');
+
+    $('html, body').stop().animate({
+      scrollTop: $(target).offset().top - 25
+    },3000);
+
+  })
+
+  $(window).scroll(function(){
+
+    let position = $(this).scrollTop();
+    
+    if(position>= 718){
+      $('#back-to-top').addClass('scrollTop');
+      
+    }
+    else{
+      $('#back-to-top').removeClass('scrollTop');
+     
+    }
+  })
+
+
   $("header, .info").ripples({
     dropRadius: 25,
     perturbance: 0.6,
